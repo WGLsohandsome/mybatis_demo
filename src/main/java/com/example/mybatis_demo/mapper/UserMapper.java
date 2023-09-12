ebf33c3999c2671006e220696b8225b8c4520dd5
汪高乐 <624917387@qq.com>
2023-09-12 20:24:01 +0800

    Mybatis test

---
+++ src/main/java/com/example/mybatis_demo/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.example.mybatis_demo.mapper;
+
+import com.example.mybatis_demo.model.UserName;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+@Mapper
+public interface UserMapper {
+    //查询所有
+    public List<UserName> getAll();
+
+    public UserName getById(@Param("uid") Integer id);
+
+    public UserName getByName(@Param("username")String username);
+}
+
---
+++ src/main/java/com/example/mybatis_demo/model/UserName.java
@@ -0,0 +1,12 @@
+package com.example.mybatis_demo.model;
+
+import lombok.Data;
+
+/**
+ * 普通实体类,用于mybatis映射
+ */
+@Data
+public class UserName {
+    private int id;
+    private  String name;
+}
