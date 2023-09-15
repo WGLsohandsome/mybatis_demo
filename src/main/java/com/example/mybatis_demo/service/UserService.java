7c55b55f5f4b824ae3806d7f558c72a59ccebd1d
汪高乐 <624917387@qq.com>
2023-09-15 11:28:41 +0800

    GIt test

--- src/main/java/com/example/mybatis_demo/service/UserService.java
+++ src/main/java/com/example/mybatis_demo/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.mybatis_demo.mapper.UserMapper;
 import com.example.mybatis_demo.model.UserName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
